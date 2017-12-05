f2891a7aaf1cf6258c1e692d7c207bf140b7b704
Pavel Levchuk <AnimeshnigX@gmail.com>
2017-12-05 15:25:06 +0300

    add base entities with one key

---
+++ src/EntityManager/EntityBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EntityManager
+{
+    /// <summary>
+    /// Base Item with ID of type <typeparamref name="TKey"/>.
+    /// </summary>
+    /// <typeparam name="TKey">Key type.</typeparam>
+    public abstract class EntityBase<TKey> : IEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        /// <summary>
+        /// Id of the item.
+        /// </summary>
+        public virtual TKey Id { get; set; }
+    }
+}
---
+++ src/EntityManager/IEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EntityManager
+{
+    /// <summary>
+    /// Interface for accessing PK.
+    /// </summary>
+    /// <typeparam name="TKey">Key type.</typeparam>
+    public interface IEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        /// <summary>
+        /// Id of the Item.
+        /// </summary>
+        TKey Id { get; set; }
+    }
+}
