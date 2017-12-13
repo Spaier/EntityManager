5ef0391686a3aadb232a2141ac91ca667950c11b
Pavel Levchuk <AnimeshnigX@gmail.com>
2017-12-13 23:31:47 +0300

    move identity code to an aspnetcore package

---
+++ src/EntityManager.AspNetCore/AdminAuthorizationHandler.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityManager.AspNetCore
+{
+    /// <summary>
+    /// Allows specified role to ignore auth handlers.
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public class AdminAuthorizationHandler<TEntity> : AuthorizationHandler<OperationAuthorizationRequirement, TEntity>
+    {
+        private readonly string _role;
+
+        /// <summary>
+        /// Create handler for given role name.
+        /// </summary>
+        /// <param name="role"></param>
+        public AdminAuthorizationHandler(string role)
+        {
+            _role = role;
+        }
+
+        /// <summary>
+        /// Handle request.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="requirement"></param>
+        /// <param name="resource"></param>
+        /// <returns>Task for handling request.</returns>
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, OperationAuthorizationRequirement requirement,
+            TEntity resource)
+        {
+            if (context.User != null && context.User.IsInRole(_role))
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
--- src/EntityManager.AspNetCore/ServiceCollectionExtensions.cs
+++ src/EntityManager.AspNetCore/ServiceCollectionExtensions.cs
@@ -19,13 +19,32 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <typeparam name="TUser"></typeparam>
         /// <typeparam name="TUserKey"></typeparam>
         /// <param name="serviceCollection"></param>
-        public static void AddEntityManagerIdentity<TUserEntity, TUserManager, TUser, TUserKey>(this IServiceCollection serviceCollection)
+        public static void AddUserIsOwnerHandler<TUserEntity, TUserManager, TUser, TUserKey>(this IServiceCollection serviceCollection)
             where TUserEntity : IUserEntity<TUser, TUserKey>
             where TUserManager : UserManager<TUser>
             where TUser : IdentityUser<TUserKey>
             where TUserKey : IEquatable<TUserKey>
         {
             serviceCollection.AddScoped<IAuthorizationHandler, UserIsOwnerAuthorizationHandler<TUserEntity, TUserManager, TUser, TUserKey>>();
+        }
+
+        /// <summary>
+        /// Adds admin auth handler for specified type.
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="serviceCollection"></param>
+        public static void AddAdminAuthHandler<TEntity>(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IAuthorizationHandler>(w => new AdminAuthorizationHandler<TEntity>(""));
+        }
+
+        /// <summary>
+        /// Adds admin auth handler for any type.
+        /// </summary>
+        /// <param name="serviceCollection"></param>
+        public static void AddAdminAuthHandler(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped(typeof(IAuthorizationHandler), typeof(AdminAuthorizationHandler<>));
         }
     }
 }
