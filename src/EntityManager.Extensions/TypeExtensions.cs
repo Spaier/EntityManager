2090f3f164ecefed71d0d6fd400df2ff5d548bf6
Pavel Levchuk <SpaierInstinct@gmail.com>
2018-07-20 03:32:34 +0300

    refactor: add internal type extensions

---
+++ src/EntityManager.Extensions/TypeExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace System
+{
+    internal static class TypeExtensions
+    {
+        internal static Type UnwrapNullableType(this Type type) => Nullable.GetUnderlyingType(type) ?? type;
+    }
+}
