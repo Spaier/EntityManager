16aed5dc02676e16d15cc4df5e1e312fd3fb55c7
Pavel Levchuk <SpaierInstinct@gmail.com>
2018-07-29 23:59:26 +0300

    style: remove unused usings

--- benchmarks/EntityManager.Benchmarks.NetCore/BuildPredicateBenchmark.cs
+++ benchmarks/EntityManager.Benchmarks.NetCore/BuildPredicateBenchmark.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Runtime.CompilerServices;
 using BenchmarkDotNet.Attributes;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
